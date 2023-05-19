cd06fcad07f145e002fd204db678f9c8dad8ea5d
Normandabald <lpassingham@hotmail.co.uk>
2023-05-19 19:49:17 +0100

    added spinners and improved config control

--- obsidisearch.py
+++ obsidisearch.py
@@ -10,22 +10,29 @@ from langchain.chains import RetrievalQA
 from langchain.document_loaders import ObsidianLoader
 from langchain.prompts import PromptTemplate
 from chromadb.utils import embedding_functions
+from langchain.embeddings import HuggingFaceEmbeddings, SentenceTransformerEmbeddings
+from halo import Halo
 
+spinner = Halo(text='Loading', spinner='dots', color='magenta')
 
 def load_config():
-    print("Loading config from config.yaml")
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     return config
 
 config = load_config()
+spinner.start("Loading embedding model...")
+embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+spinner.stop()
 
 
 # Create the vector database only if it doesn't already exist. (You will need to delete the directory to recreate it.)
 if not os.path.exists(config["persistence_directory"]):
-    print("Persisted vector database not found. Creating a new one.")
+    print("Persisted vector database not found.")
+    spinner.start("Creating a new one...")
     loader = ObsidianLoader(config["obisidian_directory"])
     documents = loader.load()
+    spinner.stop()
     print(f"Loaded {len(documents)} notes from obsidian")
 
     # Split the documents into chunks of 300 characters
@@ -33,16 +40,17 @@ if not os.path.exists(config["persistence_directory"]):
     texts = text_splitter.split_documents(documents)
 
     # Create a vector database from the documents
-    vectordb = Chroma.from_documents(documents=texts, persist_directory=config["persistence_directory"])
+    spinner.start("Creating vector database")
+    vectordb = Chroma.from_documents(documents=texts, embedding_function=embeddings, persist_directory=config["persistence_directory"])
+    spinner.stop()
     # Persist the vector database to disk
     vectordb.persist()
     vectordb = None
 else:
-    print(f"Persisted vector database found at: {config['persistence_directory']}")
+    print(f"Persisted vector database found at: `{config['persistence_directory']}`")
 
 # Load the vector database from disk
-print("Loading vector database from disk")
-vectordb = Chroma(persist_directory=config["persistence_directory"])
+vectordb = Chroma(persist_directory=config["persistence_directory"], embedding_function=embeddings)
 
 # Create a custom prompt.
 prompt = PromptTemplate(
@@ -50,11 +58,21 @@ prompt = PromptTemplate(
 )
 chain_type_kwargs = {"prompt": prompt}
 
-llm = ChatOpenAI(
-        openai_api_key=config["openai_api_key"],
-        model_name=config["openai_model"],
-        temperature=config["temperature"],
-        )
+# Gives the option to use a chat model instead of a language model.
+# non-chat models are generally cheaper to run but may not provide as good results.
+# see https://openai.com/pricing for help choosing a model.
+if config["use_chat_model"] is True:
+    llm = ChatOpenAI(
+            openai_api_key=config["openai_api_key"],
+            model_name=config["openai_model"],
+            temperature=config["temperature"],
+            )
+else:
+    llm = OpenAI(
+            openai_api_key=config["openai_api_key"],
+            model_name=config["openai_model"],
+            temperature=config["temperature"],
+            )
 
 qa = RetrievalQA.from_chain_type(
     llm=llm,
@@ -63,6 +81,12 @@ qa = RetrievalQA.from_chain_type(
     chain_type_kwargs=chain_type_kwargs
     )
 
-query = input("Enter a query:")
-print("Running query...")
-print(qa.run(query))
+cont = "y"
+while cont == "y":
+    query = input("Enter a query: ")
+    spinner.start("Searching...")
+    result = qa.run(query)
+    spinner.succeed(result)
+    print("--------------------")
+    if input("Enter a new query: "):
+        cont = "y"
