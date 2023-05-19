206b4718e8ac0bebe92765dbc10ac5267c471507
Normandabald <lpassingham@hotmail.co.uk>
2023-05-19 20:35:48 +0100

    refactored into functions

--- obsidisearch.py
+++ obsidisearch.py
@@ -1,62 +1,77 @@
-import sys
 import os
 import yaml
 from langchain.vectorstores import Chroma
-from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import ObsidianLoader
 from langchain.prompts import PromptTemplate
-from chromadb.utils import embedding_functions
-from langchain.embeddings import HuggingFaceEmbeddings, SentenceTransformerEmbeddings
+from langchain.embeddings import SentenceTransformerEmbeddings
 from halo import Halo
 
-spinner = Halo(text='Loading', spinner='dots', color='magenta')
 
-def load_config():
+def load_config() -> dict:
+    """Loads the config.yaml file and returns it as a dictionary."""
     with open("config.yaml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
     return config
 
-config = load_config()
-spinner.start("Loading embedding model...")
-embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-spinner.stop()
 
-
-# Create the vector database only if it doesn't already exist. (You will need to delete the directory to recreate it.)
-if not os.path.exists(config["persistence_directory"]):
-    print("Persisted vector database not found.")
-    spinner.start("Creating a new one...")
+@Halo(text='Loading documents from obsidian', spinner='dots1', color='magenta')
+def load_notebooks() -> list:
+    """Loads the documents from the obsidian vault and returns them as a list."""
     loader = ObsidianLoader(config["obisidian_directory"])
     documents = loader.load()
-    spinner.stop()
-    print(f"Loaded {len(documents)} notes from obsidian")
+    return documents
 
-    # Split the documents into chunks of 300 characters
+
+@Halo(text='Creating vector database', spinner='dots1', color='magenta')
+def create_vector_database(documents, embeddings) -> None:
+    """Creates a vector database from the documents and persists it."""
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=0)
     texts = text_splitter.split_documents(documents)
-
-    # Create a vector database from the documents
-    spinner.start("Creating vector database")
     vectordb = Chroma.from_documents(documents=texts, embedding_function=embeddings, persist_directory=config["persistence_directory"])
-    spinner.stop()
-    # Persist the vector database to disk
     vectordb.persist()
     vectordb = None
+    return
+
+
+@Halo(text='Loading vector database', spinner='dots1', color='magenta')
+def load_vector_database() -> Chroma:
+    """Loads the vector database from the persistence directory and returns it."""
+    vectordb = Chroma(persist_directory=config["persistence_directory"], embedding_function=embeddings)
+    return vectordb
+
+
+@Halo(text='Creating prompt', spinner='dots1', color='magenta')
+def create_prompt() -> dict:
+    """Creates the prompt for the language model and returns it as a dictionary."""
+    prompt = PromptTemplate(
+        template=config["prompt"], input_variables=["context", "question"]
+    )
+    return {"prompt": prompt}
+
+
+@Halo(text='Searching...', spinner='dots1', color='magenta')
+def run_query(query):
+    print(qa.run(query))
+    return
+
+
+
+config = load_config()
+embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+if not os.path.exists(config["persistence_directory"]):
+    print("Persisted vector database not found.")
+    documents = load_notebooks()
+    create_vector_database(documents, embeddings)
 else:
     print(f"Persisted vector database found at: `{config['persistence_directory']}`")
 
-# Load the vector database from disk
 vectordb = Chroma(persist_directory=config["persistence_directory"], embedding_function=embeddings)
-
-# Create a custom prompt.
-prompt = PromptTemplate(
-    template=config["prompt"], input_variables=["context", "question"]
-)
-chain_type_kwargs = {"prompt": prompt}
+chain_type_kwargs = create_prompt()
 
 # Gives the option to use a chat model instead of a language model.
 # non-chat models are generally cheaper to run but may not provide as good results.
@@ -81,12 +96,11 @@ qa = RetrievalQA.from_chain_type(
     chain_type_kwargs=chain_type_kwargs
     )
 
-cont = "y"
-while cont == "y":
-    query = input("Enter a query: ")
-    spinner.start("Searching...")
-    result = qa.run(query)
-    spinner.succeed(result)
+cont = True
+query = input("Enter a query: ")
+while cont:
+    run_query(query)
     print("--------------------")
-    if input("Enter a new query: "):
-        cont = "y"
+    query = input("\nEnter a new query: ")
+    if not query:
+        cont = False
